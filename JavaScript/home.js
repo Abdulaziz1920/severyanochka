cf9986fb0d3a29ca09f7b8127f1a209315aedd25
Abdulaziz1920 <abdulazizgit1920@gmail.com>
2023-05-26 19:07:49 +0300

    arrays

---
+++ JavaScript/home.js
@@ -0,0 +1,690 @@
+/*---------------ARRAY---------------*/
+const product = [
+  {
+    id: 11,
+    name: "Kebab",
+    category: "Food",
+    description:
+      "Шашлык считают блюдом кавказской кухни, и производят его название от тюрского слова 'шиш' - вертел. ",
+    price: "1.00$",
+    rating: 5,
+    discount: 10,
+    image: "./Assets/images/Arr_image/12.png",
+  },
+  {
+    id: 22,
+    name: "Juice",
+    category: "Drink",
+    description:
+      "Сок — жидкий пищевой продукт, полученный в результате отжима съедобных спелых плодов овощных или фруктовых культур.",
+    price: "8.00$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/23.png",
+  },
+  {
+    id: 23,
+    name: "Pizza",
+    category: "Fast Food",
+    description:
+      "Пи́цца итальянское национальное блюдо в виде круглой открытой лепешки.",
+    price: "7.00$",
+    rating: 4.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/24.png",
+  },
+  {
+    id: 24,
+    name: "Apelsin",
+    category: "Juice",
+    description:
+      "Апельси́н — плодовое дерево; вид рода Цитрус семейства Рутовые, а также плод этого дерева.",
+    price: "10.00$",
+    rating: 4.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/25.png",
+  },
+  {
+    id: 25,
+    name: "Peach",
+    category: "Fruits",
+    description:
+      "Пе́рсик обыкновенный, или Персик — растение из подрода Миндаль рода Слива семейства Розовые.",
+    price: "8.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/26.png",
+  },
+  {
+    id: 26,
+    name: "Fish",
+    category: "Food",
+    description:
+      "Рыбы — обширная группа водных челюстноротых позвоночных животных, ранее считавшаяся надклассом.",
+    price: "27.00$",
+    rating: 4,
+    discount: 0,
+    image: "./Assets/images/Arr_image/27.png",
+  },
+  {
+    id: 27,
+    name: "Ananas",
+    category: "Fruits",
+    description:
+      "Сезон ананасов длится круглый год. А это значит - в любое время вы можете купить его.",
+    price: "13.50$",
+    rating: 4.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/28.png",
+  },
+  {
+    id: 28,
+    name: "Coffee",
+    category: "Tea",
+    description:
+      "Ко́фе — напиток из жареных и перемолотых зёрен кофейного дерева или кофейного куста.",
+    price: "81.50$",
+    rating: 4,
+    discount: 20,
+    image: "./Assets/images/Arr_image/29.png",
+  },
+  {
+    id: 29,
+    name: "Spaghetti",
+    category: "Lunch",
+    description:
+      "Spaghetti is a type of pasta made from wheat flour and water, usually served with sauce.",
+    price: "96.50$",
+    rating: 4,
+    discount: 50,
+    image: "./Assets/images/Arr_image/30.png",
+  },
+  {
+    id: 30,
+    name: "Pizza",
+    category: "Lunch",
+    description:
+      "Pizza is a popular dish made from dough, sauce, cheese, and toppings.",
+    price: "95.50$",
+    rating: 4,
+    discount: 0,
+    image: "./Assets/images/Arr_image/31.png",
+  },
+  {
+    id: 31,
+    name: "Grilled Cheese",
+    category: "Lunch",
+    description:
+      "A grilled cheese sandwich is a hot sandwich made from cheese and bread.",
+    price: "10.50$",
+    rating: 5,
+    discount: 50,
+    image: "./Assets/images/Arr_image/32.png",
+  },
+  {
+    id: 32,
+    name: "Steak",
+    category: "Dinner",
+    description:
+      "Steak is a popular dinner dish made from a cut of beef that is usually grilled or pan-fried.",
+    price: "63.50$",
+    rating: 4.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/33.png",
+  },
+  {
+    id: 33,
+    name: "Salmon",
+    category: "Dinner",
+    description:
+      "Salmon is a type of fish that is often grilled or baked and served as a main course.",
+    price: "8.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/34.png",
+  },
+  {
+    id: 34,
+    name: "Chicken Alfredo",
+    category: "Dinner",
+    description:
+      "Chicken Alfredo is a pasta dish made with chicken, fettuccine pasta",
+    price: "94.50$",
+    rating: 4,
+    discount: 20,
+    image: "./Assets/images/Arr_image/35.png",
+  },
+  {
+    id: 35,
+    name: "Lasagna",
+    category: "Dinner",
+    description:
+      "Lasagna is a type of pasta dish made with layers of pasta, sauce, cheese, and meat or vegetables.",
+    price: "70.50$",
+    rating: 5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/36.png",
+  },
+  {
+    id: 36,
+    name: "Chocolate Chip Cookies",
+    category: "Dessert",
+    description:
+      "Chocolate Chip Cookies are a type of cookie made from flour, sugar, butter, and chocolate chips.",
+    price: "98.50$",
+    rating: 3.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/37.png",
+  },
+];
+const news = [
+  {
+    id: 1,
+    name: "Apple",
+    category: "Fruit",
+    description: "АКЦИЯ Свинина туш. Тактика 325г 36шт/уп",
+    price: "89.50$",
+    rating: 5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/1.png",
+  },
+  {
+    id: 2,
+    name: "Banana",
+    category: "Fruit",
+    description:
+      "A long, curved fruit with a yellow skin and sweet, creamy white flesh.",
+    price: "71.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/2.png",
+  },
+  {
+    id: 3,
+    name: "Bread",
+    category: "Bakery",
+    description:
+      "A staple food made from flour, water, and yeast, baked into a loaf.",
+    price: "88.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/3.png",
+  },
+  {
+    id: 4,
+    name: "Broccoli",
+    category: "Vegetable",
+    description:
+      "A green vegetable with a tree-like appearance, known for its high nutrient content.",
+    price: "94.50$",
+    rating: 4.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/5.png",
+  },
+  {
+    id: 5,
+    name: "Carrot",
+    category: "Vegetable",
+    description:
+      "A root vegetable with a crunchy texture and a sweet, earthy flavor.",
+    price: "82.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/6.png",
+  },
+  {
+    id: 6,
+    name: "Cheese",
+    category: "Dairy",
+    description:
+      "A dairy product2 made from milk, with a variety of textures and flavors.",
+    price: "91.50$",
+    rating: 3.5,
+    discount: 50,
+    image: "./Assets/images/Arr_image/7.png",
+  },
+  {
+    id: 7,
+    name: "Chicken",
+    category: "Meat",
+    description:
+      "A type of poultry, often used as a source of protein in meals.",
+    price: "68.50$",
+    rating: 3.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/8.png",
+  },
+  {
+    id: 8,
+    name: "Chocolate",
+    category: "Dessert",
+    description:
+      "A sweet food made from cacao beans, used in a variety of desserts and confections.",
+    price: "61.50$",
+    rating: 4.5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/9.png",
+  },
+  {
+    id: 9,
+    name: "Coffee",
+    category: "Dessert",
+    description:
+      "A dark, bitter beverage made from roasted coffee beans, typically used as a pick-me-up.",
+    price: "98.50$",
+    rating: 5,
+    discount: 20,
+    image: "./Assets/images/Arr_image/10.png",
+  },
+  {
+    id: 10,
+    name: "Egg",
+    category: "Dairy",
+    description:
+      "A oval-shaped reproduct2ive body produced by birds, often used as a source of protein in meals.",
+    price: "79.50$",
+    rating: 4.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/11.png",
+  },
+];
+const recently = [
+  {
+    id: 12,
+    name: "Grapes",
+    category: "Fruit",
+    description:
+      "A small, sweet fruit that grows in clusters, often used in wine-making.",
+    price: "61.50$",
+    rating: 4,
+    discount: 20,
+    image: "./Assets/images/Arr_image/13.png",
+  },
+  {
+    id: 13,
+    name: "Hamburger",
+    category: "Fast food",
+    description:
+      "A sandwich made with a patty of ground beef, served on a bun with various toppings.",
+    price: "87.50$",
+    rating: 5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/14.png",
+  },
+  {
+    id: 14,
+    name: "Hot dog",
+    category: "Fast food",
+    description:
+      "A type of sausage served in a long roll, often used as a quick and convenient food.",
+    price: "84.50$",
+    rating: 4.5,
+    discount: 50,
+    image: "./Assets/images/Arr_image/15.png",
+  },
+  {
+    id: 15,
+    name: "Ice cream",
+    category: "Dessert",
+    description:
+      "A frozen dessert made from cream, sugar, and flavorings, available in a variety of flavors.",
+    price: "68.50$",
+    rating: 4.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/16.png",
+  },
+  {
+    id: 16,
+    name: "Lemon",
+    category: "Fruit",
+    description:
+      "A yellow citrus fruit with a tart, acidic flavor, often used in cooking and baking.",
+    price: "74.50$",
+    rating: 3.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/17.png",
+  },
+  {
+    id: 17,
+    name: "Eggs",
+    category: "Dairy",
+    description:
+      "Eggs are oval-shaped reproduct2ive structures produced by female birds and some reptiles.",
+    price: "70.50$",
+    rating: 4.5,
+    discount: 50,
+    image: "./Assets/images/Arr_image/18.png",
+  },
+  {
+    id: 18,
+    name: "Honey",
+    category: "Dairy",
+    description:
+      "Honey is a sweet, sticky liquid made by bees from the nectar of flowers.",
+    price: "87.50$",
+    rating: 4,
+    discount: 0,
+    image: "./Assets/images/Arr_image/19.png",
+  },
+  {
+    id: 19,
+    name: "Ice Cream",
+    category: "Dessert",
+    description:
+      "Ice cream is a frozen dessert made from cream, sugar, and flavorings.",
+    price: "83.50$",
+    rating: 4.5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/20.png",
+  },
+  {
+    id: 20,
+    name: "Jelly",
+    category: "Fruit",
+    description:
+      "Jelly is a sweet spread made from fruit juice and sugar, often used as a topping for toast or pastries.",
+    price: "76.50$",
+    rating: 5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/21.png",
+  },
+  {
+    id: 21,
+    name: "Ketchup",
+    category: "Dairy",
+    description:
+      "Ketchup is a tangy sauce made from tomatoes, vinegar, sugar, and spices.",
+    price: "77.50$",
+    rating: 5,
+    discount: 0,
+    image: "./Assets/images/Arr_image/22.png",
+  },
+];
+const specialOffers = [
+  {
+    id: 1,
+    title: "Оформите карту «Северяночка»",
+    text: "И получайте бонусы при покупке в магазинах и на сайте",
+    image: "Assets/images/home/card.png",
+  },
+  {
+    id: 2,
+    title: "Покупайте акционные товары",
+    text: "И получайте вдвое больше бонусов",
+    image: "Assets/images/home/baskets.png",
+  },
+];
+const article = [
+  {
+    date: "05.03.2023",
+    article_title:
+      "Режим использования масок и перчаток на территории магазинов",
+    article_text:
+      'Подробная информация о режимах использования масок и перчаток на территории магазинов "ЛЕНТА". Информация обновляется каждый будний день.',
+    image: "Assets/images/home/Status.png",
+  },
+  {
+    date: "05.03.2023",
+    article_title:
+      "Режим использования масок и перчаток на территории магазинов",
+    article_text:
+      'Подробная информация о режимах использования масок и перчаток на территории магазинов "ЛЕНТА". Информация обновляется каждый будний день.',
+    image: "Assets/images/home/Status2.png",
+  },
+  {
+    date: "05.03.2023",
+    article_title:
+      "Режим использования масок и перчаток на территории магазинов",
+    article_text:
+      'Подробная информация о режимах использования масок и перчаток на территории магазинов "ЛЕНТА". Информация обновляется каждый будний день.',
+    image: "Assets/images/home/Status3.png",
+  },
+];
+/*---------------ARRAY---------------*/
+/*---------------PROMOTION---------------*/
+const categories = [
+  ...new Set(
+    product.map((item) => {
+      return item;
+    })
+  ),
+];
+document.getElementById("root").innerHTML = categories
+  .map((item) => {
+    let { image, description, price, discount } = item;
+    if (item.discount > 0) {
+      return `
+        <div class="promotion__cards">
+          <img class="img" src="${image}" alt="" />
+          <p class="discount Text_S">-${discount}%</p>
+          <div class="promotion__price">
+            <div class="left">
+              <h4 class="Text-Bold_M">44,50 ₽</h4>
+              <p class="Text_XS">С картой</p>
+            </div>
+            <div class="right">
+              <h4 class="Text_S">${price}</h4>
+              <p class="Text_XS">Обычная</p>
+            </div>
+          </div>
+          <div class="promotion__text">
+            <p class="Text_S">${description}</p>
+          </div>
+          <div class="promotion__star">
+            ${getRating(item.rating)}
+          </div>
+          <button class="Text_S promotion_btn">В корзину</button>
+        </div>`;
+    }
+  })
+  .slice(-10)
+  .join("");
+function getRating(rating) {
+  let res = "";
+  let star_count = 0;
+  let full_star = parseInt(rating);
+  let rest_star = rating - full_star;
+  star_count = full_star;
+  res = Array(full_star)
+    .fill("<img src='Assets/icons/rating/full.svg'>")
+    .join("");
+  if (0.25 <= rest_star && rest_star <= 0.5) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/half.svg'>";
+  }
+  if (0.5 < rest_star) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/full.svg'>";
+  }
+  free_star = 5 - star_count;
+  res += Array(free_star)
+    .fill("<img src='Assets/icons/rating/empty.svg'>")
+    .join("");
+  return res;
+}
+/*---------------PROMOTION---------------*/
+/*---------------NEW---------------*/
+const highsRatingCards = (product) => {
+  product.sort((a, b) => b.rating - a.rating);
+
+  return product;
+};
+highsRatingCards(product).slice(-6);
+
+const products2 = [
+  ...new Set(
+    news.map((item) => {
+      return item;
+    })
+  ),
+];
+document.getElementById("newsCards").innerHTML = products2
+  .map((item) => {
+    let { image, description, price } = item;
+    if (item.discount > 0) {
+      return `
+      <div class="new__card">
+      <img class="img" src=${image} alt="" />
+      <h4 class="Text-Bold_M">${price}</h4>
+      <p class="Text_S">${description}</p>
+      <div class="new_star">
+      ${getRating(item.rating)}
+      </div>
+      <button class="Text_S new_btn">В корзину</button>
+    </div>`;
+    }
+  })
+  .slice(-10)
+  .join("");
+function getRating(rating) {
+  let res = "";
+  let star_count = 0;
+  let full_star = parseInt(rating);
+  let rest_star = rating - full_star;
+  star_count = full_star;
+  res = Array(full_star)
+    .fill("<img src='Assets/icons/rating/full.svg'>")
+    .join("");
+  if (0.25 <= rest_star && rest_star <= 0.5) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/half.svg'>";
+  }
+  if (0.5 < rest_star) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/full.svg'>";
+  }
+  free_star = 5 - star_count;
+  res += Array(free_star)
+    .fill("<img src='Assets/icons/rating/empty.svg'>")
+    .join("");
+  return res;
+}
+/*---------------NEW---------------*/
+/*---------------BEFORE---------------*/
+const lowerRatingCards = (product) => {
+  product.sort((a, b) => a.rating - b.rating);
+
+  return product;
+};
+lowerRatingCards(product).slice(-6);
+
+const products3 = [
+  ...new Set(
+    recently.map((item) => {
+      return item;
+    })
+  ),
+];
+document.getElementById("before").innerHTML = products3
+  .map((item) => {
+    let { image, description, price } = item;
+    if (item.discount > 0) {
+      return `
+      <div class="new__card">
+      <img class="img" src=${image} alt="" />
+      <h4 class="Text-Bold_M">${price}</h4>
+      <p class="Text_S">${description}</p>
+      <div class="new_star">
+      ${getRating(item.rating)}
+      </div>
+      <button class="Text_S new_btn">В корзину</button>
+    </div>`;
+    }
+  })
+  .slice(-20)
+  .join("");
+function getRating(rating) {
+  let res = "";
+  let star_count = 0;
+  let full_star = parseInt(rating);
+  let rest_star = rating - full_star;
+  star_count = full_star;
+  res = Array(full_star)
+    .fill("<img src='Assets/icons/rating/full.svg'>")
+    .join("");
+  if (0.25 <= rest_star && rest_star <= 0.5) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/half.svg'>";
+  }
+  if (0.5 < rest_star) {
+    star_count++;
+    res += "<img src='Assets/icons/rating/full.svg'>";
+  }
+  free_star = 5 - star_count;
+  res += Array(free_star)
+    .fill("<img src='Assets/icons/rating/empty.svg'>")
+    .join("");
+  return res;
+}
+/*---------------BEFORE---------------*/
+/*---------------SPECIAL---------------*/
+const products4 = [
+  ...new Set(
+    specialOffers.map((item) => {
+      return item;
+    })
+  ),
+];
+document.getElementById("special").innerHTML = specialOffers
+  .map((item) => {
+    let { image, title, text } = item;
+    if (item.id === 1) {
+      return `
+      <div class="special__item lefts">
+      <div class="special__offers__title">
+        <h1 class="Header_S">${title}</h1>
+        <p class="Text_S">
+         ${text}
+        </p>
+      </div>
+      <div class="special__offers__image">
+        <img src=${image} alt="" />
+      </div>
+    </div>`;
+    } else if (item.id === 2) {
+      return `<div class="special__item rights">
+        <div class="special__offers__title">
+          <h1 class="Header_S">${title}</h1>
+          <p class="Text_S">${text}</p>
+        </div>
+        <div class="special__offers__image">
+          <img src=${image} alt="" />
+        </div>
+      </div>`;
+    }
+  })
+  .join("");
+/*---------------SPECIAL---------------*/
+/*---------------ARTICLE---------------*/
+const products5 = [
+  ...new Set(
+    article.map((item) => {
+      return item;
+    })
+  ),
+];
+document.getElementById("article").innerHTML = article
+  .map((item) => {
+    let { date, article_text, article_title, image } = item;
+
+    return `<div class="articles__cards">
+    <img src=${image} alt="" />
+    <h5 class="Text_XS">${date}</h5>
+    <h2 class="Header_XS">${article_title}</h2>
+    <p class="Text_S">
+      ${article_text}
+    </p>
+    <button class="Text_S articles_btn">Подробнее</button>
+  </div>
+      `;
+  })
+  .slice(-3)
+  .join("");
+/*---------------ARTICLE---------------*/
+
+let hamburger = document.querySelector(".hamburger");
+let wrapper = document.querySelector(".wrapper");
+
+hamburger.addEventListener("click", function () {
+  wrapper.classList.toggle("show");
+});
